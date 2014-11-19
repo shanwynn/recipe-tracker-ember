7521756bdcf2a3328f1b8bc5ac80097c28915e5d
Shannon Wynn <shannonwynn@mac.com>
2014-11-19 17:18:29 -0500

    authentication working

--- js/app.js
+++ js/app.js
@@ -1,9 +1,82 @@
 App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
+App.Firebase = new Firebase('https://dazzling-torch-1926.firebaseio.com');
 
 App.ApplicationAdapter = DS.FirebaseAdapter.extend({
-	firebase: new Firebase('https://dazzling-torch-1926.firebaseio.com')
+	firebase: App.Firebase
+});
+
+App.ApplicationController = Ember.Controller.extend({
+    // used to show, or not show, the log out button
+    isLoggedIn: false,
+    // when a user enters the app unauthenticated, the transition
+    // to where they are going is saved off so it can be retried
+    // when they have logged in.
+    savedTransition: null,
+
+    login: function() {
+      this.setProperties({ savedTransition: null, isLoggedIn: true });
+    },
+
+    logout: function() {
+      this.set('isLoggedIn', false);
+    }
+});
+
+
+App.ApplicationRoute = Ember.Route.extend({
+  beforeModel: function(transition) {
+    var applicationController = this.controllerFor('application');
+    if (localStorage.authToken) {
+      this.controllerFor('application').login();
+    } else {
+      this.controllerFor('application').logout();
+    }
+  },
+  actions: {
+    logout: function() {
+      this.controllerFor('application').logout();
+      delete localStorage.authToken;
+      this.transitionTo('login');
+    }
+  }
+});
+
+App.AuthenticatedRoute = Ember.Route.extend({
+  beforeModel: function(transition) {
+    var applicationController = this.controllerFor('application');
+    if (localStorage.authToken !== App.Firebase.getAuth().token) {//Yay! actual verification
+      applicationController.set('savedTransition', transition);
+      this.transitionTo('login');
+    } else {
+      this.controllerFor('application').login();
+    }
+  }
+});
+
+App.LoginRoute = Ember.Route.extend({
+    actions: {
+        login: function () {
+            var loginController = this.controllerFor('login');
+            var username = loginController.get('username');
+            var password = loginController.get('password');
+            var that = this;
+            App.Firebase.authWithPassword({
+                email: username,
+                password: password
+            }, function (error, authData) {
+                console.log(authData);
+                if (authData) {
+                    localStorage.authToken = authData.token;
+                    that.transitionTo('index');
+                } else {
+                    console.warn(error);
+                    loginController.set('error', error.message);
+                }
+            });
+        }
+    }
 });
 
 App.CATEGORIES = [
@@ -18,6 +91,7 @@ App.Router.map(function() {
   this.route ('edit_recipe', {path: 'recipes/:id/edit'});
   this.route ('new_recipe', {path: 'recipes/new'});
   this.route ('category', {path: 'recipes'});
+  this.route ('login', {path: 'login'});
 });
 
 App.Recipe = DS.Model.extend({
@@ -29,7 +103,7 @@ App.Recipe = DS.Model.extend({
   directions:  DS.attr('string')
 });
 
-App.IndexRoute = Ember.Route.extend({
+App.IndexRoute = App.ApplicationRoute.extend({
   model: function() {
     return this.store.find('recipe');
   }
@@ -51,7 +125,7 @@ App.NewRecipeController = Ember.Controller.extend({
   }
 });
 
-App.RecipeRoute = Ember.Route.extend({
+App.RecipeRoute = App.ApplicationRoute.extend({
   model: function(params) {
     return this.store.find('recipe', params.id);
   }
@@ -99,7 +173,7 @@ App.RecipeController = Ember.ObjectController.extend({
   }
 });
 
-App.EditRecipeRoute = Ember.Route.extend({
+App.EditRecipeRoute = App.AuthenticatedRoute.extend({
   model: function(params) {
     return this.store.find('recipe', params.id);
   }
@@ -118,7 +192,7 @@ App.EditRecipeController = Ember.ObjectController.extend({
   }
 });
 
-App.CategoryRoute = Ember.Route.extend({
+App.CategoryRoute = App.ApplicationRoute.extend({
   model: function (params) {
     return this.store.find('recipe');
   }
@@ -144,7 +218,7 @@ App.CategoryController = Ember.ArrayController.extend({
   }.property('category', 'model')
 });
 
-App.NewRecipeIndexRoute = App.Route.extend({
+App.NewRecipeIndexRoute = App.AuthenticatedRoute.extend({
     model: function () {
         return this.store.find('ingredient');
     }
